930887908ea8430653cf1aee96643bc6b407e460
Kornel Tako <takokornel@gmail.com>
2020-06-14 18:34:44 +0200

    Add general migration, models, controllers

---
+++ database/migrations/2020_06_14_161834_create_organizations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrganizationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('organizations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->index();
+            $table->string('image');
+            $table->string('address');
+            $table->string('postcode');
+            $table->string('city');
+            $table->string('country');
+            $table->point('geolocation')->spatialIndex();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('organizations');
+    }
+}
